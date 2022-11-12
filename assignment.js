0e534d99a42dc93145fa036d9bd324e25dc126c9
Surajubarhande <surajalk1996@gmail.com>
2022-11-12 22:35:56 +0530

    added by suraj

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 let a=prompt("enter first number");
     a=parseInt(a);
     b=prompt("enter second number");
-    b=parseInt(a);
+    b=parseInt(b);
     total=a+b;
     document.write("addition of two number="+total)
